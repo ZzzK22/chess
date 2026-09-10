e541bdc3941cf7e5c4cd31aca205d3ddf0268986
Nicholas Peck <nicholas@peckfam.com>
2026-09-10 11:22:04 -0600

    valid ChessBoard for real this time

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -20,7 +20,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
     /**
