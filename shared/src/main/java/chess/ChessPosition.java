041ba0d4864e7cfea6a9d5b80640ac630a49f442
Nicholas Peck <nicholas@peckfam.com>
2026-09-10 11:00:05 -0600

    removed useless code in ChessPosition

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -11,8 +11,8 @@ public class ChessPosition {
     private final int row;
     private final int col;
     public ChessPosition(int row, int col) {
-        this.row = row - 1;
-        this.col = col - 1;
+        this.row = row;
+        this.col = col;
     }
 
     /**
@@ -20,7 +20,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        return row + 1;
+        return row;
     }
 
     /**
@@ -28,6 +28,6 @@ public class ChessPosition {
      * 1 codes for the left column
      */
     public int getColumn() {
-        return col + 1;
+        return col;
     }
 }
